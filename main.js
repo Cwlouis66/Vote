3fa0286921afd758db3a778f7e9d97ba61916f7c
Cwlouis66 <cwlouis66@gmail.com>
2024-02-06 15:04:57 +0800

    voting success

---
+++ main.js
@@ -0,0 +1,240 @@
+let WALLET_CONNECTED ='';
+let CONTRACT_ADDRESS = '0x0a2884BBc7C5CaaF49d9acDEe46C37D2860cF34e';
+let contractAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "string[]",
+          "name": "_candidateNames",
+          "type": "string[]"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_durationInMinutes",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        }
+      ],
+      "name": "addCandidate",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "candidates",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "voteCount",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAllVotesOfCandiates",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "voteCount",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Voting.Candidate[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getRemainingTime",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getVotingStatus",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "vote",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "voters",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "votingEnd",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "votingStart",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+  const connectMetamask = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    WALLET_CONNECTED = await signer.getAddress();
+    var element = document.getElementById("metamaskNotification");
+    element.innerHTML = "Metamask is connected to: " + WALLET_CONNECTED;
+  };
+
+  const getAllCandidates = async () => {
+    var p3 = document.getElementById("p3");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contractAbi, signer);
+    p3.innerHTML = "Loading...";
+    var candidates = await contractInstance.getAllVotesOfCandiates();
+    console.log(candidates);
+    var table = document.getElementById("myTable");
+
+    for(let i = 0; i < candidates.length; i++){
+        var row = table.insertRow();
+        var idCell = row.insertCell();
+        var descCell = row.insertCell();
+        var statusCell = row.insertCell();
+
+        idCell.innerHTML = i+1;
+        descCell.innerHTML = candidates[i].name;
+        statusCell.innerHTML = candidates[i].voteCount;
+    }
+
+    p3.innerHTML = "Candidates are all up to date";
+  };
+
+  const voteStatus = async () => {
+    if (WALLET_CONNECTED != 0) {
+      var status = document.getElementById("status");
+      var remainingTime = document.getElementById("time");
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contractAbi, signer);
+      const curStatus = await contractInstance.getVotingStatus();
+      const time = await contractInstance.getRemainingTime();
+      status.innerHTML = curStatus == 1 ? "Voting is open" : "Voting has finished";
+      remainingTime.innerHTML = `Remaining time is ${parseInt(time, 16)} seconds`;
+    
+    }
+
+    else{
+        var status = document.getElementById("status");
+        status.innerHTML = "Please connect to metamask first";
+    }
+  };
+
+  const addVote = async() => {
+    if(WALLET_CONNECTED != 0) {
+        var name = document.getElementById("vote");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, contractAbi, signer);
+        var cand = document.getElementById("cand");
+        cand.innerHTML = "Please wait, adding a vote in the smart contract";
+        const tx = await contractInstance.vote(name.value);
+        await tx.wait();
+        cand.innerHTML = "Vote added !!!";
+    }
+    else {
+        var cand = document.getElementById("cand");
+        cand.innerHTML = "Please connect metamask first";
+    }
+}
